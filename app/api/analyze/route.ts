77038bbece6c5b1b1472687e7e0c68b65dd1f7a2
miaoficiial-arch <miaoficiial@gmail.com>
2026-08-25 04:21:47 +0200

    Update route.ts

--- app/api/analyze/route.ts
+++ app/api/analyze/route.ts
@@ -139,7 +139,7 @@ La respuesta debe ser detallada y personalizada.
 `;
 
     const hfResponse = await fetch(
-"https://router.huggingface.co/hf-inference/models/google/gemma-3-4b-it",
+https://router.huggingface.co/hf-inference/models/HuggingFaceTB/SmolLM2-1.7B-Instruct,
       {
         method: "POST",
         headers: {
